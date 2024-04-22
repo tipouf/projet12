f8bb8c37fb97e7d8d636af60777bc4a7785abaf5
cindon <nicolascindon@gmail.com>
2024-04-22 16:44:40 +0200

    sidebar ok

---
+++ src/assets/index.js
@@ -0,0 +1,6 @@
+import strong from "./svg/strong.svg";
+import bike from "./svg/bike.svg";
+import swim from "./svg/swim.svg";
+import yoga from "./svg/yoga.svg";
+
+export { strong, bike, swim, yoga }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,16 @@
+import "./Header.scss";
+import { Link } from "react-router-dom";
+
 const Header = () => {
   return (
-    <div className="Header">
-      <h1>Header</h1>
+    <div className="header">
+      <img src="src/assets/logo.png" alt="logo" />
+      <nav className="nav">
+        <Link to="/">Accueil</Link>
+        <Link to="/profil">Profil</Link>
+        <Link to="/settings">Réglage</Link>
+        <Link to="/communiversity">Communauté</Link>
+      </nav>
     </div>
   );
 };
